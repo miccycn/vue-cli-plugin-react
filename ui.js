1a6ee1663288503df1a833a4f144ffac6ee51cb8
weiyuanyue <millywey@qq.com>
2018-11-26 16:59:09 +0800

    完善 ui 选项的读写

--- ui.js
+++ ui.js
@@ -1,14 +1,43 @@
 module.exports = api => {
 	api.describeConfig({
-		// 唯一的配置 ID
 		id: 'vueconf.demo',
-		// 展示名称
 		name: 'vueconf',
-		// 该配置所有可能的文件
 		files: {
 			vue: {
 				js: ['vue.config.js']
 			}
+		},
+		onRead: ({
+			data
+		}) => {
+			return {
+				prompts: [{
+						name: 'themeColor',
+						type: 'color',
+						message: '选择一种配置的颜色',
+						default: '#4DBA87',
+						value: data.vue.pluginOptions && data.vue.pluginOptions.vueconf && data.vue.pluginOptions.vueconf.themeColor
+					},
+					{
+						name: 'welcome',
+						type: 'input',
+						message: '项目介绍',
+						description: '请输入这个项目的介绍',
+						default: 'Learn React',
+						value: data.vue.pluginOptions && data.vue.pluginOptions.vueconf && data.vue.pluginOptions.vueconf.welcome
+					}
+				]
+			}
+		},
+		onWrite: async ({
+			api,
+			prompts
+		}) => {
+			const result = {}
+			for (const prompt of prompts) {
+				result[`pluginOptions.vueconf.${prompt.id}`] = await api.getAnswer(prompt.id)
+			}
+			api.setData('vue', result)
 		}
 	})
 }
