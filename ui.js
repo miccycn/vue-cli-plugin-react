34006253bb9a1bd7b7485b6bc8620e569659c0e6
weiyuanyue <millywey@qq.com>
2018-11-26 16:58:08 +0800

    增加ui.js

--- ui.js
+++ ui.js
@@ -1,3 +1,14 @@
-module.exports = (api) => {
-
-};
+module.exports = api => {
+	api.describeConfig({
+		// 唯一的配置 ID
+		id: 'vueconf.demo',
+		// 展示名称
+		name: 'vueconf',
+		// 该配置所有可能的文件
+		files: {
+			vue: {
+				js: ['vue.config.js']
+			}
+		}
+	})
+}
