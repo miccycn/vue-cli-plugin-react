f4caa05b186235717069631bc381a12c23c8c735
weiyuanyue <millywey@qq.com>
2018-11-26 16:46:15 +0800

    在插件中扩展 package.json 安装 react 和 react-dom，渲染模板

--- generator/index.js
+++ generator/index.js
@@ -1,3 +1,19 @@
 module.exports = (api, option, rootOptions) => {
+	// 扩展 package.json
+	api.extendPackage({
+		dependencies: {
+			"react": "^16.0.0",
+			"react-dom": "^16.0.0"
+		},
+		devDependencies: {
+			"babel-preset-react-app": "^6.0.0"
+		},
+		eslintConfig: {
+			"extends": [
+				"plugin:react/recommended",
+			]
+		}
+	});
 
-}
+	api.render('./template');
+};
